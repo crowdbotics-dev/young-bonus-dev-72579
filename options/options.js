9d555cb98a8019a506e95c4e607901c8e666279a
Crowdbotics <engineering@crowdbotics.com>
2023-03-28 06:11:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "young-bonus-dev-72579",
+  url: "https://young-bonus-dev-72579.botics.co",
+  api: "https://young-bonus-dev-72579.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
